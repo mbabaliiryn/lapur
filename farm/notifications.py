4ef8b8f7ce04f124ff5d15068f38bf10ce90d06d
mbabali iryn <mbabaliiryn@gmail.com>
2019-10-08 13:36:43 +0300

    add new user joined notification

--- farm/notifications.py
+++ farm/notifications.py
@@ -1,4 +1,18 @@
 
+from django_rq import job  
 
 
 
+
+
+
+@job
+def send_new_user_joined(instance):
+    instance = clean_instance(instance, get_user_model())
+    subject = "{} joined ".format(instance.display_name)
+    to = ORGANISATION_STAFF_UPDATE_EMAIL_RECIPIENTS
+    ctx = {
+        'user': instance,
+        'user_url': '%s/people/%s/' % (ORGANISATION_URL, instance.username)
+    }
+    send_mail(subject, 'organisation/email/new_user', to, ctx)
